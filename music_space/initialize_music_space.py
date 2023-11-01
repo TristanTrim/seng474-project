accf798f2eb82b507e05a35ca8890f35ee509223
Will Murray <williammurray29@gmail.com>
2023-10-31 18:11:03 -0700

    music space interface implemented

---
+++ music_space/initialize_music_space.py
@@ -0,0 +1,103 @@
+import hdf5_getters as h5
+import matplotlib.pyplot as plt 
+import numpy as np
+import os
+from sklearn.neighbors import KDTree
+import time 
+
+
+def grab_song(path, row_ID):    
+
+    """
+    Extract the features from a song file (.h5 format) given a file path
+    file path must be relative to music space
+
+    returns:
+        X: vector of song features
+        song_ID: unique string identifier for the song
+    """
+    song_file = h5.open_h5_file_read(path)
+
+    song_ID = h5.get_song_id(song_file)
+    X = np.array(
+        [
+            h5.get_danceability(song_file),
+            h5.get_energy(song_file),
+            h5.get_key(song_file),
+            h5.get_mode(song_file),
+            h5.get_tempo(song_file) 
+        ]
+        )
+        
+    X = np.concatenate(
+        (
+            X,
+            np.array(h5.get_segments_start(song_file)[:100])
+        ),
+        axis=0
+    )
+
+    song_file.close()
+    return [song_ID, X]
+
+
+def num_features():
+    return grab_song("MillionSongSubset\B\D\A\TRBDAID128F92E88C6.h5", 0)[1].shape[0]
+    
+
+def initialize_music_space(title = "simple"):
+
+    """
+    1. Initialize and save the music space to music_space/embedding.
+    Embedding is a numpy ndarray, saved as a .npy file
+    The features in the music space are selected in the function grab song
+
+    2. Initialize and save a vector of song_ID
+    """
+
+    root = "MillionSongSubset"
+    processed = 0
+    # Initialize an empty numpy ndarray with 10000 rows and n columns. 
+    # Here, assuming a song has 'n' features.
+    music_space = []
+    song_IDs = []
+    d = num_features()
+    print(f"songs processed: {processed}",end='\r')
+
+    for prefix_1 in ['A']:
+        child = root + '/' + prefix_1
+
+        for prefix_2 in os.listdir(child):
+            g_child = child + '/' + prefix_2
+            
+            for prefix_3 in os.listdir(g_child):
+                gg_child = g_child + '/' + prefix_3
+            
+                for song_file_path in os.listdir(gg_child):
+        
+                    song_ID, X = grab_song(path= gg_child + '/' + song_file_path, row_ID=processed)
+
+                    if len(X) == d:
+                        music_space.append(X)
+                        song_IDs.append(song_ID)
+                    processed += 1
+
+                    print(f"songs processed: {processed}",end='\r')
+
+                    break
+
+    song_IDs = np.array(song_IDs)
+    music_space = np.array(music_space)
+
+    np.save(
+        file = os.getcwd() + '/embeddings/' + title + "_music_space",
+        arr = music_space
+        )
+
+    np.save(
+        file = os.getcwd() + '/embeddings/' + title + "_song_ID_space",
+        arr = song_IDs
+        )
+
+
+initialize_music_space("v1")
--- music_space/music_space.py
+++ music_space/music_space.py
@@ -1,34 +1,54 @@
-import hdf5_getters as h5
-import matplotlib.pyplot as plt 
 import numpy as np
+from sklearn.neighbors import KDTree
 import time
-import os
+import matplotlib.pyplot as plt
 
-def grab_song(path):    
-    song_file = h5.open_h5_file_read(path)
+class MusicSpace:
 
-    song_file.close()
+    def __init__(self, title):
+        self.feature_vectors = np.load('embeddings/' + title + '_music_space.npy')
+        self.song_IDs = np.load('embeddings/' + title + '_song_ID_space.npy', allow_pickle=True)
+        self.KD_TREE = KDTree(data= self.feature_vectors, leaf_size= 1) #tune leaf size??
+
+    def vector_to_songID(self, song_vector):
+        """returns the song_ID for the given song_vector"""
+
+        distances = np.linalg.norm(self.feature_vectors - song_vector, axis=1)
+        min_distance_index = np.argmin(distances)        
+        song_id = self.song_IDs[min_distance_index]
+        
+        return song_id
+
+    def songID_to_vector(self, song_id):
+        """returns the song_vector for the given song_id"""
+
+        index = np.where(self.song_IDs == song_id)[0]
+        if index.size == 0:
+            return None
+        else:
+            vector = self.feature_vectors[index[0]]
+            return vector
+
+    def NN(self, song_vector):
+        """returns the nearest neighbour of a song vector"""
+
+        dist, index = self.KD_TREE.query(X = song_vector.reshape(1,-1), k = 2)
+        print(f"distance = {dist[0][1]}")
+        return(self.feature_vectors[index[0][1]])
+
+#Example Usage
+
+#initialize music space
+t0 = time.time()
+title = 'v1' #using the first music space version
+MS = MusicSpace(title) #initialize the space
+print(f"initialization time: {time.time() - t0}")
 
 
-def initialize_song_space():
+#Try a nearest Neighbour query on the 4th song in the music space
+X1 = MS.feature_vectors[3]
 
-    """
-    returns a pandas dataframe where each row is the selected attributes of the song.
-    This fuction depends of the Million Song Subset being accessible 
+t0 = time.time()
+MS.NN(X1)
+print(f"kNN query time: {time.time() - t0}")
 
-    """
-
-    root = "MillionSongSubset"
-    capital_letters = [chr(i) for i in range(65, 91)]
-    for pref1 in ['A', 'B']:
-        for pref2 in capital_letters:
-            for pref3 in capital_letters:
-                directory = root + '/' + pref1 + '/' + pref2 + '/' + pref3
-                for file_suffix in os.listdir(directory):
-                    path = directory + '/' + file_suffix
-                    grab_song(path)
-                
-                print(f"{len(os.listdir(directory))} songs processed")
-
-
-all_songs()
